33a2c01434f7024a8f39b65ae28c51ed0b5caf57
hussein <ahmedhus1984@gmail.com>
2024-11-23 09:26:25 +0800

    tidied up code

--- main.py
+++ main.py
@@ -20,52 +20,77 @@ def mysqlCursor(conn):
         exit(1)
     return myCursor
 
-def mysqlCreate(table, cursor):
-    serial, hostname, type, shipDate, warrExp, site, location, status, department, model=input('enter serial, hostname, type, shipDate, warrExp, site, location, status, department, model seperated by space:\n').split(',')
+def mysqlCreate(cursor, table):
+    serial, hostname, type, shipDate, warrExp, site, location, status, department, model, brand=input('enter serial, hostname, type, shipDate, warrExp, site, location, status, department, model, brand seperated by comma:\n').split(',')
     print(serial+'\n'+hostname+'\n'+type+'\n'+shipDate+'\n'+warrExp+'\n'+site+'\n'+location+'\n'+status+'\n'+department+'\n'+model)
-    cursor.execute(f'insert into {table} (serial, hostname, type, shipDate, warrExp, site, location, status, department, model) values("{serial}", "{hostname}", "{type}", "{shipDate}", "{warrExp}", "{site}", "{location}", "{status}", "{department}", "{model}");')
+    cursor.execute(f'insert into {table} (serial, hostname, type, shipDate, warrExp, site, location, status, department, model, brand) values("{serial}", "{hostname}", "{type}", "{shipDate}", "{warrExp}", "{site}", "{location}", "{status}", "{department}", "{model}", "{brand}");')
 
-def mysqlRead(table, serial, cursor):
-    cursor.execute(f'select * from {table} where serial="{serial}"')
+def mysqlRead(cursor, table, readParam):
+    blah=readColumns(cursor, table)
+    cursor.execute(f'select * from {table} where serial=\'{readParam}\'')
+    blah2=[]
     for i in cursor:
         for j in i:
-            print(j)
+            blah2.append(j)
+    print()
+    for i in range(0, len(blah), 1):
+        print(f'{blah[i]: <12}: {blah2[i]}')
+    print()
+
+
+def readColumns(cursor, table):
+    cursor.execute(f'select column_name from information_schema.columns where table_schema=\'bcinv\' and table_name=\'{table}\'')
+    blah=cursor.fetchall()
+    blah2=[]
+    for i in blah:
+        blah2.append(i[0])
+    return blah2
 
 def mysqlConnClose(conn):
     conn.close()
 
 def choiceSelect(conn, cursor):
-    a=input('enter table to view record; s for systems, l for logs, u for users, x to exit: ')
-    match a:
-        case 's':
-            a='systems'
-        case 'l':
-            a='logs'
-        case 'u':
-            a='users'
-        case 'x':
-            return a
-        case _:
-            print('wrong choice, only s, l or u allowed. ')
-            exit(2)
-    b=input('enter c, r, u or d for create, read, update or delete respectively: ')
-    match b:
-        case 'c':
-            mysqlCreate(a, cursor)
-            conn.commit()
-        case 'r':
-            c=input('enter serial number: ')
-            mysqlRead(a, c, cursor)
-        case _:
-            print('wrong choice, only c, r, u or d allowed.')
-            exit(3)
-    return b
+    while True:
+        table=input('enter option to view a table; s for systems, l for logs, u for users, x to exit: ')
+        match table:
+            case 's':
+                table='systems'
+                break
+            case 'l':
+                table='logs'
+                break
+            case 'u':
+                table='users'
+                break
+            case 'x':
+                return table
+            case _:
+                print('wrong choice, only s, l or u allowed. ')
+    while True:
+        operation=input('enter c, r, u, d to create, read, update, delete records, x to exit: ')
+        match operation:
+            case 'c':
+                mysqlCreate(cursor, table)
+                conn.commit()
+                break
+            case 'e':
+                blah=readColumns(cursor, table)
+                print(blah)
+                break
+            case 'r':
+                readParam=input('enter serial number: ')
+                mysqlRead(cursor, table, readParam)
+                break
+            case 'x':
+                return operation
+            case _:
+                print('wrong choice, only c, r, u, d or x is allowed.')
 
 def main():
     conn, myCursor=mysqlConn()
     a=''
     while True:
-        choiceSelect(conn, myCursor)
+        a=choiceSelect(conn, myCursor)
         if a=='x':
             print('bye!')
             mysqlConnClose(conn)
