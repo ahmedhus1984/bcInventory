c294834736560c3b19296ea489d53541da7c89ca
hussein <ahmedhus1984@gmail.com>
2024-12-23 00:48:09 +0800

    swap tested, all working fine...need to include function for adding users and adding systems

--- main.py
+++ main.py
@@ -47,102 +47,138 @@ def getDetailsSwap():
     return hostnameReturn, hostnameIssue, email, site, date
 
 def deviceIssue(conn, cursor, hostname, email, date):
-    #new user check begins
-    result=0
+    #issue check begins
     cursor.execute(f'select department from systems where hostname=\'{hostname}\' and status=\'pending_deployment\'')
-    try:
-        deviceDepartment=(list(cursor))[0][0].strip().lower()
-    except IndexError:
-        print(f'host \'{hostname}\' or status \'pending_deployment\' not found in systems table')
-        return
     result=cursor.rowcount
     if result==0:
-        cursor.execute(f'select * from systems where hostname=\'{hostname}\'')
-        result=cursor.rowcount
-        if result==0:
-            print('hostname not found...please contact database admin')
-        else:
-            print('hostname found, but not available for deployment...please contact database admin')
+        print(f'either host \'{hostname}\' not found or host \'{hostname}\' has a status other than \'pending_deployment\' in systems table\n\nplease contact database admin')
         return
-    else:
-        print(f'{result} hostname(s) named \'{hostname}\', from \'{deviceDepartment}\' department found')
-    
-    #double confirm hostname availability in case the status is reflected correctly in the systems table
+    contents=cursor.fetchall()
+    deviceDepartment=contents[0][0].strip().lower()
+    #double confirm hostname availability in case the status is reflected incorrectly as 'pending_deployment'in the systems table
     cursor.execute(f'select * from currown where hostname=\'{hostname}\'')
     result=cursor.rowcount
     if result==0:
         print('hostname currently not in use, available for deployment')
     else:
-        print(f'{result} hostname(s) named \'{hostname}\' currently in use by {cursor}')
-    #double confirm hostname availability in case the status is reflected correctly in the systems table
+        contents=cursor.fetchall()
+        print(f'{result} hostname(s) named \'{hostname}\' currently in use by {contents[0][0]}...please contact database admin')
+        return
+    #double confirm hostname availability in case the status is reflected incorrectly as 'pending_deployment'in the systems table
 
     cursor.execute(f'select * from users where user=\'{email}\'')
-    userDepartment=(list(cursor))[0][1].strip().lower()
     result=cursor.rowcount
     if result==0:
         print('user not found...please contact database admin')
         return
     else:
+        contents=cursor.fetchall()
+        userDepartment=contents[0][1].strip().lower()
         print(f'{result} user(s) with username, \'{email}\' found')
-    cursor.execute(f'select * from users where user=\'{email}\' and department=\'{userDepartment}\'')
-    result=cursor.rowcount
-    if result==0:
-        print('department for user not found...please contact database admin')
-        return
-    else:
-        print(f'{result} user(s) named \'{email}\' from department(s) named \'{userDepartment}\' found')
     if userDepartment==deviceDepartment:
         print('\ndepartments for user and device match...device can be issued to user')
     else:
         print('\ndepartments for user and device are different...please contact database admin')
         return
-    # new user check ends
+    #issue check ends
 
     print(f'\nsystems record for \'{hostname}\' before insertion of new record:')
     mysqlRead(cursor, 'systems', hostname)
     print(f'\ncurrent owner record for \'{email}\' before insertion of new record:')
     mysqlRead(cursor, 'currown', hostname)
+
     #modifications to systems and currown tables for device issuance
     cursor.execute(f'insert into currown values(\'{date}\', \'{hostname}\', \'{email}\')')
     cursor.execute(f'update systems set site=null, status=\'deployed\' where hostname=\'{hostname}\'')
+    #modifications to systems and currown tables for device issuance
+
     print(f'\nsystems record for \'{hostname}\' after insertion of new record:')
     mysqlRead(cursor, 'systems', hostname)
     print(f'\ncurrent owner record for \'{email}\' after insertion of new record:')
     mysqlRead(cursor, 'currown', hostname)
+
+    #save modifications
     commit=input('\ncommit? y/n: ')
     if(commit=='y'):
         conn.commit()
         print('\nmodifications saved!\n')
     else:
         print('\nno updates to database\n')
+    #save modifications
 
 def deviceReturn(conn, cursor, hostname, email, site, date):
+    #return check begins
+    cursor.execute(f'select department from systems where hostname=\'{hostname}\' and status=\'deployed\'')
+    result=cursor.rowcount
+    if result==0:
+        print(f'either host \'{hostname}\' not found or host \'{hostname}\' has a status other than \'deployed\' in systems table\n\nplease contact database admin')
+        return
+    contents=cursor.fetchall()
+    deviceDepartment=contents[0][0].strip().lower()
+    #double confirm hostname availability in case the status is reflected incorrectly as 'pending_deployment'in the systems table
+    cursor.execute(f'select * from currown where hostname=\'{hostname}\'')
+    result=cursor.rowcount
+    if result==1:
+        print('hostname currently deployed, available for return')
+    else:
+        print(f'hostname \'{hostname}\' has no records for current owner...please contact database admin')
+        return
+    #double confirm hostname availability in case the status is reflected incorrectly as 'pending_deployment'in the systems table
+
+    cursor.execute(f'select * from users where user=\'{email}\'')
+    result=cursor.rowcount
+    if result==0:
+        print('user not found...please contact database admin')
+        return
+    else:
+        contents=cursor.fetchall()
+        userDepartment=contents[0][1].strip().lower()
+        print(f'{result} user(s) with username, \'{email}\' found')
+    if userDepartment==deviceDepartment:
+        print('\ndepartments for user and device match...device can be returned by user')
+    else:
+        print('\ndepartments for user and device are different...please contact database admin')
+        return
+    #return check ends
+
     print(f'\nsystems info before update: ')
     mysqlRead(cursor, 'systems', hostname)
     print(f'\ncurrown info before update: ')
     mysqlRead(cursor, 'currown', hostname)
     print(f'\nprevown info before update: ')
     mysqlRead(cursor, 'prevown', hostname)
+
+    #modifications to systems and currown tables for device issuance 
     cursor.execute(f'insert into prevown select * from currown where hostname=\'{hostname}\' and user=\'{email}\'')
     cursor.execute(f'update prevown set date=\'{date}\' where hostname=\'{hostname}\' and user=\'{email}\'')
     cursor.execute(f'delete from currown where hostname=\'{hostname}\' and user=\'{email}\'')
     cursor.execute(f'update systems set site=\'{site}\', status=\'pending_deployment\' where hostname=\'{hostname}\'')
+    #modifications to systems and currown tables for device issuance
+    
     print(f'\nsystems info after update: ')
     mysqlRead(cursor, 'systems', hostname)
     print(f'\ncurrown info after update: ')
     mysqlRead(cursor, 'currown', hostname)
     print(f'\nprevown info after update: ')
     mysqlRead(cursor, 'prevown', hostname)
+
+    #save modifications
     commit=input('\ncommit? y/n: ')
     if(commit=='y'):
         conn.commit()
         print('\nmodifications saved!\n')
+        return 0
     else:
         print('\nno updates to database\n')
+        return 1
+    #save modifications
 
 def deviceSwap(conn, cursor, hostnameReturn, hostnameIssue, email, site, date):
-    deviceReturn(conn, cursor, hostnameReturn, email, site, date)
-    deviceIssue(conn, cursor, hostnameIssue, email, date)
+    a=deviceReturn(conn, cursor, hostnameReturn, email, site, date)
+    if a==0:
+        deviceIssue(conn, cursor, hostnameIssue, email, date)
+    else:
+        print('swap safely aborted')
 
 # def devIssueRevert(conn, cursor, site, location, status):
 #     print()
@@ -232,6 +268,7 @@ enter an option:
                     
 i for device issuance
 r for device return
+s for device swap
 x to exit
 
 ''')
@@ -251,6 +288,8 @@ x to exit
                 hostnameIssue, email, site, date=getDetailsReturn()
                 deviceReturn(conn, cursor, hostnameIssue, email, site, date)
             case 's':
+                hostnameReturn, hostnameIssue, email, site, date=getDetailsSwap()
+
                 deviceSwap(conn, cursor, hostnameReturn, hostnameIssue, email, site, date)
             # case 'r':
             #     devIssueRevert(conn, cursor, 'napier', 'server room', 'pending_deployment')
